cc51580945930d442180cdbda3544686645aa5fc
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-07-31 14:06:33 +0700

    test: CORS

--- src/app/page.js
+++ src/app/page.js
@@ -1,4 +1,19 @@
 "use client";
+import { useAdminGetSession } from "medusa-react";
 export default function Home() {
-  return <div></div>;
+  const { data, isLoading } = useAdminGetSession();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          if (!isLoading) {
+            console.log(data);
+          }
+        }}
+      >
+        Click
+      </button>
+    </div>
+  );
 }
