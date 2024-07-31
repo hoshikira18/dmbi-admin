80279ad00fc673c1b5b995c5dbfec2e314986379
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-07-31 14:40:20 +0700

    test: CORS

--- src/app/page.js
+++ src/app/page.js
@@ -1,15 +1,27 @@
 "use client";
-import { useAdminGetSession } from "medusa-react";
+import { useAdminGetSession, useAdminLogin } from "medusa-react";
 export default function Home() {
   const { data, isLoading } = useAdminGetSession();
+  const adminLogin = useAdminLogin();
 
+  const handleLogin = () => {
+    adminLogin.mutate(
+      {
+        email: "dmbi@gmail.com",
+        password: "12345",
+      },
+      {
+        onSuccess: ({ user }) => {
+          console.log(user);
+        },
+      },
+    );
+  };
   return (
     <div>
       <button
         onClick={() => {
-          if (!isLoading) {
-            console.log(data);
-          }
+          handleLogin();
         }}
       >
         Click
