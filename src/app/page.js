28c46dfa1b7ff3c2a815915b832e3368fd22b345
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-08-02 16:02:49 +0700

    ui: add button

--- src/app/page.js
+++ src/app/page.js
@@ -1,7 +1,13 @@
+'use client';
+import { Button } from '@/components/ui/button';
+
 const HomePage = () => {
     return (
         <div>
             <h1>Home Page</h1>
+            <button className="btn btn-primary bg-yellow-400 hover:bg-yellow-300">
+                Click me
+            </button>
         </div>
     );
 };
