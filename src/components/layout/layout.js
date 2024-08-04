55daafe3808fa4453e9b385d817a7565287814ea
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-08-04 17:53:55 +0700

    add: sticky layout

--- src/components/layout/layout.js
+++ src/components/layout/layout.js
@@ -58,7 +58,7 @@ function Layout({ children }) {
     return (
         <div className="grid min-h-screen w-full md:grid-cols-[220px_1fr] lg:grid-cols-[280px_1fr]">
             <div className="hidden border-r bg-muted/40 md:block">
-                <div className="flex h-full max-h-screen flex-col gap-2">
+                <div className="sticky top-0 flex h-full max-h-screen flex-col gap-2">
                     <div className="flex h-14 items-center border-b px-4 lg:h-[60px] lg:px-6">
                         <Link
                             href="/"
@@ -99,8 +99,8 @@ function Layout({ children }) {
                     </div>
                 </div>
             </div>
-            <div className="flex flex-col">
-                <header className="flex h-14 items-center gap-4 border-b bg-muted/40 px-4 lg:h-[60px] lg:px-6">
+            <div className="flex flex-col bg-white">
+                <header className="sticky top-0 z-50 flex h-14 items-center gap-4 border-b bg-white px-4 lg:h-[60px] lg:px-6">
                     <Sheet>
                         <SheetTrigger asChild>
                             <Button
@@ -135,7 +135,7 @@ function Layout({ children }) {
                             </div>
                         </SheetContent>
                     </Sheet>
-                    <div className="w-full flex-1">
+                    <div className="w-full flex-1 bg-white">
                         <form>
                             <div className="relative">
                                 <Search className="absolute left-2.5 top-2.5 h-4 w-4 text-muted-foreground" />
@@ -176,7 +176,7 @@ function Layout({ children }) {
                         </DropdownMenuContent>
                     </DropdownMenu>
                 </header>
-                <main className="flex flex-1 flex-col gap-4 p-4 lg:gap-6 lg:p-6">
+                <main className="flex flex-1 flex-col gap-4 overflow-scroll p-4 lg:gap-6 lg:p-6">
                     {children}
                 </main>
             </div>
