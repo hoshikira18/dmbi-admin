385a209e0f37c269b1eecfcb61b01916ea39f7cf
Tran Van Khuyen <81480245+hoshikira18@users.noreply.github.com>
2024-08-08 18:40:44 +0700

    feat: blog management

---
+++ src/app/blog/[id]/page.js
@@ -0,0 +1,7 @@
+import PostDetailTemplate from '@/components/blog/post-detail-template';
+
+const PostDetailPage = ({ params }) => {
+    return <PostDetailTemplate params={params} />;
+};
+
+export default PostDetailPage;
